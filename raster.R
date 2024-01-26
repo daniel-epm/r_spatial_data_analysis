71c5daedbf902a0a4f2a2b0c6f8b58b05c21d908
Daniel E. Parra M <depm1123@gmail.com>
2024-01-25 20:11:08 -0500

    new file for raster operations lessons

---
+++ raster.R
@@ -0,0 +1,79 @@
+
+## -- Módulo 24: Dados espaciais e mapas
+
+
+
+# 18: Operações com raster - juntar dados ---------------------------------
+
+
+setwd("D:/Daniel/courses/curso_R/modulo24-dados_espaciais_e_mapas/")
+
+library(raster)
+library(ggplot2)
+
+
+  # Juntar arquivos tif do estado de Sergipe
+
+
+tifs <- list.files("input/raster/srtm_sergipe/", pattern = "*.tif")
+
+
+raster_list <- list()
+
+# Use a for loop to read each file and store in the list
+for (i in seq_along(tifs)) {
+  # Construct the variable name (r1, r2, ..., r6)
+  variable_name <- paste0("r", i)
+  
+  # Read the TIFF file using the raster function
+  raster_obj <- raster(file.path("input/raster/srtm_sergipe", tifs[i]))
+  
+  # Assign the raster object to the variable
+  assign(variable_name, raster_obj)
+  
+  # Save the raster object in the list
+  raster_list[[i]] <- raster_obj
+}
+
+
+for (i in raster_list) {
+  print(raster::origin(i))
+}
+
+
+plot(r1)
+plot(r2)
+plot(r3)
+plot(r4)
+plot(r5)
+plot(r6)
+
+
+
+sergipe <- raster::merge(r1,r2, r3, r4, r5, r6)  # Pode-se usar a função raster::mosaic() também
+
+plot(sergipe)
+
+
+brasil <- geobr::read_state(code_state = 'all')
+
+brasil[brasil$name_state == 'Sergipe', ]
+
+st_sergipe <- geobr::read_state(code_state = 28)
+
+
+
+ggplot() +
+  geom_raster(data = as.data.frame(sergipe$layer, xy = TRUE),
+              aes(x = x, y = y, fill = layer)) +
+  geom_sf(data = st_sergipe, fill = NA, lwd = 0.7, color= 'white')
+
+
+
+
+
+
+
+
+
+
